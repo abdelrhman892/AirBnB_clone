449417b9a431f631c4e80f4c2e729562beaeaf8e
Nairaalmelegy <nairamohamed384@yahoo.com>
2024-02-06 01:05:21 +0200

    to_dict update

--- models/base_model.py
+++ models/base_model.py
@@ -5,9 +5,9 @@ import datetime
 
 class BaseModel:
     def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         return ("[{}] ({}) {}"
