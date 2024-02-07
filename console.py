2456c6296b69d62eb4a3ab3b59af3dcbb7eda329
abdelrhman <115238439+abdelrhman892@users.noreply.github.com>
2024-02-07 19:23:22 +0200

    fixing some errors

--- console.py
+++ console.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 import cmd
+import sys
+from models.base_model import BaseModel
+from models.user import User
+from models.amenity import Amenity
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.review import Review
+from datetime import datetime
 from models.engine.file_storage import FileStorage
 from shlex import shlex
-from models import storage, base_model, user, city, place, review, state, amenity
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
