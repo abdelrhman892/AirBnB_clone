ecf23cc74a8580ae8f8de3b3c860f588dd4f7103
abdelrhman <115238439+abdelrhman892@users.noreply.github.com>
2024-02-09 01:34:59 +0200

    fixing some errors

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,7 +11,7 @@ class FileStorage:
         return self.__objects
 
     def new(self, obj):
-        key = (obj.__class__.__name__, obj.id,)
+        key = "{}_{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
