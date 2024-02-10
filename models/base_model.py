b8de2189c916731514d851fa11b0aa5bbea7f37f
abdelrhman <115238439+abdelrhman892@users.noreply.github.com>
2024-02-10 19:25:28 +0200

    fixing some errors

--- models/base_model.py
+++ models/base_model.py
@@ -16,8 +16,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
-            if not hasattr(self, 'updated_at'):
-                self.updated_at = self.created_at
+            self.updated_at = datetime.datetime.now()
             storage.new(self)
 
     def __str__(self):
