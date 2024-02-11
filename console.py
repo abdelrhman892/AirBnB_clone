5931fa46664457f8530527686d33fba2d103ab97
Nairaalmelegy <nairamohamed384@yahoo.com>
2024-02-11 14:50:05 -0800

    done console finally

--- console.py
+++ console.py
@@ -196,7 +196,7 @@ class HBNBCommand(cmd.Cmd):
         storage.save()
 
     def help_update(self):
-        """help command for update p"""
+        """help command for update"""
         print("update command to update an instance")
 
     def analyze_parameter_value(self, value, class_name, attribute_name):
